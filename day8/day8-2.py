66b868f86fe1facd980822dd5ddb8850e2638cf9
Martijn <martijn@baulds.nl>
2022-12-08 23:22:04 +0100

    p2 solution

--- day8/day8-2.py
+++ day8/day8-2.py
@@ -25,3 +25,30 @@ for i, row in enumerate(trees):
     p2 = max(p2, score)
 
 print(f"p1: {p1}, p2: {p2}")
+
+
+#part2 only, original: https://github.com/hyper-neutrino/advent-of-code/blob/main/2022/day08p2.py
+grid = [list(map(int, line)) for line in open("day8/input.txt").read().splitlines()]
+t = 0
+for r in range(len(grid)):
+    for c in range(len(grid[r])):
+        k = grid[r][c]
+        L = R = U = D = 0
+        for x in range(c - 1, -1, -1):
+            L += 1
+            if grid[r][x] >= k:
+                break
+        for x in range(c + 1, len(grid[r])):
+            R += 1
+            if grid[r][x] >= k:
+                break
+        for x in range(r - 1, -1, -1):
+            U += 1
+            if grid[x][c] >= k:
+                break
+        for x in range(r + 1, len(grid)):
+            D += 1
+            if grid[x][c] >= k:
+                break
+        t = max(t, U * D * L * R)
+print(t)
