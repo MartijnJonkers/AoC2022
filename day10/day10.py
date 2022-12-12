5004f304d141303b2c2a9bd765c873d50b4cf8e8
Martijn <martijn@baulds.nl>
2022-12-12 15:08:44 +0100

    day 10 part 2

--- day10/day10.py
+++ day10/day10.py
@@ -15,12 +15,20 @@ for l in range(0,len(input)):
 
   cycle = sum(cycles[:l+1])                           # compute cycle after instruction
   for i in range(len(signal)+1, cycle+1):               # compute signal for each cycle in instruction
-    signal[i] = i * X
+    signal[i] = [X,i * X]
 
   X = sum(addx[:l+1]) + 1                             # X gets updated after the instruction
 
-# part 1: compute sum 0f 20,60,100,140,140,220 signal cycle
+# part 1: compute sum of 20,60,100,140,140,220 signal cycle
 sum = 0
 for s in range(20,221,40):
-  sum += signal[s]
+  sum += signal[s][1]
 print(sum)
+
+# part 2: sprite position and pixel draw cycle
+result = [['   ' for i in range(0,40)] for i in range(0,6)]
+for i in range(1, len(signal)+1):
+  if ((i-1)%40) in range(signal[i][0]-1, signal[i][0]+2):
+    result[int((i-1) / 40)][(i-1) % 40] = '###'
+for row in result:
+  print("".join(row))
